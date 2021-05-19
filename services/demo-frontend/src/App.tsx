1d2e96e3592577d78a8c5bafb0c544a02ece49b1
Johannes Feig <johannes.feig@gofore.com>
2021-05-19 13:16:05 +0300

    first web demo version

---
+++ services/demo-frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Widget from 'rasa-webchat';
+
+import logo from './logo.svg';
+import './App.css';
+
+const onSocketEvent={
+  'bot_uttered': () => console.log('the bot said something'),
+  'connect': () => console.log('connection established'),
+  'disconnect': () => console.log('disconnect'),
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Widget
+          initPayload={"/get_started"}
+          socketUrl={"http://localhost:5005"}
+          socketPath={"/socket.io/"}
+          onSocketEvent={onSocketEvent}
+          customData={{"language": "en"}} // arbitrary custom data. Stay minimal as this will be added to the socket
+          title={"Title"}
+        />
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ services/demo-frontend/src/types.d.ts
@@ -0,0 +1,1 @@
+declare module 'rasa-webchat';
