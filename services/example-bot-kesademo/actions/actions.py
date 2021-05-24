4d70cd88d6e4405a906b95219d066dc8c3ddcd48
Mika Mikkola <mika.mikkola@gofore.com>
2021-05-24 16:40:10 +0300

    small improvements

--- services/example-bot-kesademo/actions/actions.py
+++ services/example-bot-kesademo/actions/actions.py
@@ -24,10 +24,24 @@ life_situation_meter_keys = [
                     "working_studying"
                 ]
 
-# todo: Add utterance to botfront? Is this way to manage api errors?
-utter_error = "En valitettavasti pysty hakemaan palveluita juuri nyt."
+# todo: Add utterance to botfront? Develope a way to manage api errors.
+utter_api_error = "En valitettavasti pysty hakemaan palveluita juuri nyt."
+utter_location_error = "En valitettavasti löytänyt aluettasi."
 
 def life_situation(**kwargs) -> dict:
+    """ Updates life situation feature vector with given features.
+
+    Parameters
+    ----------
+    params : dict
+        a dictionary which contains key value pair for life situation.
+
+    Returns
+    -------
+    dict
+        Returns life situation feature vector as a dictionary.
+    """
+
     feats = {}
     for arg in kwargs:
         if kwargs[arg] == None:
@@ -37,10 +51,40 @@ def life_situation(**kwargs) -> dict:
     return feats
 
 class ShowServices(Action):
+    """
+    Returns service recommendations based on slot values collected by the bot.
+
+    Methods
+    -------
+    validate_feat(tracker, name)
+        Validates given slot value.
+
+    validate_location(self, dispatcher, tracker)
+        Validates location slot value.
+
+    run(dispatcher, tracker, domain)
+        Fetches service recommendations through an api.
+
+    """
+
+
     def name(self):
         return "action_show_services"
 
     def validate_feat(self, tracker, name):
+        """
+        Will check if slot exists, and if its value is convertable to int.
+
+        Parameters
+        ----------
+        name : str
+            Name of the feature/meter.
+
+        Returns
+        -------
+        int
+            Life situation feature/meter value.
+        """
         try:
             value = int(tracker.get_slot(name))
         except:
@@ -48,16 +92,29 @@ class ShowServices(Action):
         return value
 
     def validate_location(self, dispatcher, tracker):
+        """
+        Will check if location slot has a value, and tries to get municipality
+        code for that location.
+
+        Returns
+        -------
+        list(str,str)
+            Municipality code and location/city.
+        """
         try:
             location = tracker.get_slot("city").lower()
             code = municipality_codes[location]
         except:
-            dispatcher.utter_message(template="En valitettavasti löytänyt aluettasi.")
+            dispatcher.utter_message(template=utter_location_error)
             code = None
         return [code, location]
 
     def run(self, dispatcher, tracker, domain):
-
+        """
+        Fetches slot values from the bot tracker store, validates slot values,
+        and calls service recommender api to fetch recommended services based on
+        the features collected and for the location observed.
+        """
         friends = self.validate_feat(tracker, "friends")
         health = self.validate_feat(tracker, "health")
         municipality_code, location = self.validate_location(dispatcher, tracker)
@@ -79,16 +136,25 @@ class ShowServices(Action):
             if response.ok:
                 services = response.json()
                 names = [service['service_name'] for service in services['recommended_services']]
+
                 dispatcher.utter_message(
-                    template=f"Alueeltasi ({location.capitalize()}) löytyy mm. seuraavia palveluita: {str(names)}")
+                    template=f"Alueeltasi ({location.capitalize()}) löytyy mm. seuraavia palveluita:")
+
+                for name in names:
+                    dispatcher.utter_message(
+                        template=f"{name}")
+
             else:
-                dispatcher.utter_message(template=utter_error)
+                dispatcher.utter_message(template=utter_api_error)
 
         except ConnectionError:
-            dispatcher.utter_message(template=utter_error)
+            dispatcher.utter_message(template=utter_api_error)
         return[]
 
 class ActionRestarted(Action):
+    """
+    Restarts bot session.
+    """
     def name(self):
         return 'action_restart_chat'
 
@@ -96,6 +162,9 @@ class ActionRestarted(Action):
         return[Restarted()]
 
 class ActionSlotReset(Action):
+    """
+    Resets all slots to an original state.
+    """
     def name(self):
         return 'action_slot_reset'
 
