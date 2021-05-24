6c2f24ac4ec4f9a2bc69bb19a8477d7e17e7f16a
Johannes Feig <johannes.feig@gofore.com>
2021-05-24 14:45:17 +0300

    add working version of WebChatStack

--- infra/aws/bin/aws.ts
+++ infra/aws/bin/aws.ts
@@ -4,6 +4,10 @@ import * as cdk from '@aws-cdk/core';
 import { BaseStack } from '../lib/base-stack';
 //import { EcsStack } from '../lib/ecs-stack';
 import { Ec2Stack } from '../lib/ec2-stack';
+import { WebChatStack } from '../lib/web-chat-stack';
+
+const region = 'eu-north-1';
+const envName = 'demo';
 
 const app = new cdk.App();
 const base = new BaseStack(app, 'BaseStack');
@@ -19,7 +23,9 @@ const ec2stack = new Ec2Stack(app, 'Ec2Stack', {
   baseVpc: base.baseVpc
 });
 
-/* new webchatStack(app, 'WebchatStack', {
-  
-})
-*/
+new WebChatStack(app, 'WebChatStack', {
+  envName,
+  env: {
+    region
+  }
+});
