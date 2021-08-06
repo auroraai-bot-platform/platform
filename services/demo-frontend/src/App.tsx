d4131eae084a5dc39570bf571420b35e734e0b9b
Johannes Feig <johannes.feig@gofore.com>
2021-08-06 10:07:49 +0300

    Revert "apply styles and convert css to scss files"
    This reverts commit e2489bf9d563309c2669d1bdd4035f9134cae340.

--- services/demo-frontend/src/App.tsx
+++ services/demo-frontend/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Widget from 'rasa-webchat';
 
 import logo from './logo.svg';
-import './App.scss';
+import './App.css';
 
 const onSocketEvent={
   'bot_uttered': () => console.log('the bot said something'),
@@ -16,15 +16,24 @@ function App() {
       <header className="App-header">
         <Widget
           initPayload={"/get_started"}
-          socketUrl={"http://localhost:5005"}
+          socketUrl={"https://api.demo.aaibot.link:5005"}
           socketPath={"/socket.io/"}
           onSocketEvent={onSocketEvent}
           customData={{"language": "fi"}} // arbitrary custom data. Stay minimal as this will be added to the socket
-          title="<strong>hh11</strong>"
-          subtitle={null}
-          inputTextFieldHint="inputTextFieldHint"
+          title={"Title"}
         />
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
