d3da6a38286175f8664feba853904e441f3f7a5d
Tuomas Paulin <tuomas.paulin@gofore.com>
2021-10-21 16:39:56 +0300

    Two types of potential tests
    snapshot tests
    manual resource check tests

--- infra/aws/test/ecs-base-stack.test.ts
+++ infra/aws/test/ecs-base-stack.test.ts
@@ -1,5 +1,4 @@
-
-import { expect as expectCDK, haveResource, countResources, countResourcesLike } from '@aws-cdk/assert';
+import { expect as expectCDK, countResources, countResourcesLike, SynthUtils } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import { EcsBaseStack } from '../lib/ecs-base-stack';
 import { RasaBot } from '../types';
@@ -19,90 +18,119 @@ const defaultRepositories: DefaultRepositories = {
 let ecrRepos: RasaBot[] = [{rasaPort: 1, actionsPort: 2, projectId: 'veryrealid', customerName: 'veryrealcustomer'}];
 
 
-test('Check base stack resources with one bot', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new EcsBaseStack(app, 'MyTestStack', {
-        envName,
-        subDomain,
-        ecrRepos,
-        domain,
-        env: {
-          region,
-          account
-        },
-        defaultRepositories
-    });
-    // THEN
-    expectCDK(stack).to(countResources('AWS::EC2::VPC', 1)
-    .and(countResources('AWS::EC2::Subnet', 4))
-    .and(countResources('AWS::EC2::RouteTable', 4))
-    .and(countResources('AWS::EC2::SubnetRouteTableAssociation', 4))
-    .and(countResources('AWS::EC2::Route', 4))
-    .and(countResources('AWS::EC2::EIP', 1))
-    .and(countResources('AWS::EC2::NatGateway', 1))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-botfront'
-    }))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-actions-veryrealcustomer'
-    }))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-rasa-veryrealcustomer'
-    }))
-    .and(countResources('AWS::ElasticLoadBalancingV2::LoadBalancer', 1))
-    .and(countResources('AWS::SecretsManager::Secret', 1))
-    .and(countResources('AWS::EC2::SecurityGroup', 1))
-    .and(countResources('AWS::Route53::RecordSet', 1))
-    );
+test('Create base-stack with one bot', () => {
+  const app = new cdk.App();
+  // WHEN
+  const teststack = new EcsBaseStack(app, 'MyTestStack', {
+    envName,
+    subDomain,
+    ecrRepos,
+    domain,
+    env: {
+      region,
+      account
+    },
+    defaultRepositories
+  });
+  // THEN
+  expectCDK(teststack).to(countResources('AWS::EC2::VPC', 1)
+  .and(countResources('AWS::EC2::Subnet', 4))
+  .and(countResources('AWS::EC2::RouteTable', 4))
+  .and(countResources('AWS::EC2::SubnetRouteTableAssociation', 4))
+  .and(countResources('AWS::EC2::Route', 4))
+  .and(countResources('AWS::EC2::EIP', 1))
+  .and(countResources('AWS::EC2::NatGateway', 1))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-botfront'
+  }))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-actions-veryrealcustomer'
+  }))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-rasa-veryrealcustomer'
+  }))
+  .and(countResources('AWS::ElasticLoadBalancingV2::LoadBalancer', 1))
+  .and(countResources('AWS::SecretsManager::Secret', 1))
+  .and(countResources('AWS::EC2::SecurityGroup', 1))
+  .and(countResources('AWS::Route53::RecordSet', 1))
+  );
 });
 
-test('Check base stack resources with two bots', () => {
-    const app = new cdk.App();
-    ecrRepos = [
-        {rasaPort: 1, actionsPort: 2, projectId: 'veryrealid', customerName: 'veryrealcustomer'},
-        {rasaPort: 3, actionsPort: 4, projectId: 'veryrealid2', customerName: 'veryrealcustomer2'}
-    ];
+test('Create base-stack with two bots', () => {
+  const app = new cdk.App();
+  ecrRepos = [
+    {rasaPort: 1, actionsPort: 2, projectId: 'veryrealid', customerName: 'veryrealcustomer'},
+    {rasaPort: 3, actionsPort: 4, projectId: 'veryrealid2', customerName: 'veryrealcustomer2'}
+  ];
 
-    // WHEN
-    const stack = new EcsBaseStack(app, 'MyTestStack', {
-        envName,
-        subDomain,
-        ecrRepos,
-        domain,
-        env: {
-          region,
-          account
-        },
-        defaultRepositories
-    });
-    // THEN
-    expectCDK(stack).to(countResources('AWS::EC2::VPC', 1)
-    .and(countResources('AWS::EC2::Subnet', 4))
-    .and(countResources('AWS::EC2::RouteTable', 4))
-    .and(countResources('AWS::EC2::SubnetRouteTableAssociation', 4))
-    .and(countResources('AWS::EC2::Route', 4))
-    .and(countResources('AWS::EC2::EIP', 1))
-    .and(countResources('AWS::EC2::NatGateway', 1))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-botfront'
-    }))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-actions-veryrealcustomer'
-    }))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-rasa-veryrealcustomer'
-    }))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-actions-veryrealcustomer2'
-    }))
-    .and(countResourcesLike('AWS::ECR::Repository', 1, {
-        'RepositoryName': 'test-rasa-veryrealcustomer2'
-    }))
-    .and(countResources('AWS::ElasticLoadBalancingV2::LoadBalancer', 1))
-    .and(countResources('AWS::SecretsManager::Secret', 1))
-    .and(countResources('AWS::EC2::SecurityGroup', 1))
-    .and(countResources('AWS::Route53::RecordSet', 1))
-    );
+  // WHEN
+  const teststack = new EcsBaseStack(app, 'MyTestStack', {
+    envName,
+    subDomain,
+    ecrRepos,
+    domain,
+    env: {
+      region,
+      account
+    },
+    defaultRepositories
+  });
+  // THEN
+  expectCDK(teststack).to(countResources('AWS::EC2::VPC', 1)
+  .and(countResources('AWS::EC2::Subnet', 4))
+  .and(countResources('AWS::EC2::RouteTable', 4))
+  .and(countResources('AWS::EC2::SubnetRouteTableAssociation', 4))
+  .and(countResources('AWS::EC2::Route', 4))
+  .and(countResources('AWS::EC2::EIP', 1))
+  .and(countResources('AWS::EC2::NatGateway', 1))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-botfront'
+  }))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-actions-veryrealcustomer'
+  }))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-rasa-veryrealcustomer'
+  }))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-actions-veryrealcustomer2'
+  }))
+  .and(countResourcesLike('AWS::ECR::Repository', 1, {
+      'RepositoryName': 'test-rasa-veryrealcustomer2'
+  }))
+  .and(countResources('AWS::ElasticLoadBalancingV2::LoadBalancer', 1))
+  .and(countResources('AWS::SecretsManager::Secret', 1))
+  .and(countResources('AWS::EC2::SecurityGroup', 1))
+  .and(countResources('AWS::Route53::RecordSet', 1))
+  );
 });
 
+// TODO: Write tests that are able to fail to succeed, no good way to do that currently
+/* test('Check teststack with same customerNames', () => {
+  const app = new cdk.App();
+  ecrRepos = [
+    {rasaPort: 1, actionsPort: 2, projectId: 'veryrealid', customerName: 'veryrealcustomer'},
+    {rasaPort: 3, actionsPort: 4, projectId: 'veryrealid', customerName: 'veryrealcustomer2'}
+  ];
+
+  // WHEN
+  const teststack = new EcsBaseStack(app, 'MyTestStack', {
+    envName,
+    subDomain,
+    ecrRepos,
+    domain,
+    env: {
+      region,
+      account
+    },
+    defaultRepositories
+  });
+  // THEN
+  expect(() =>  {
+
+  })
+  expect(() => {
+       SynthUtils.synthesize(teststack)
+   }).toThrowError()
+});
+ */
---
+++ infra/aws/test/ecs-bf-stack.test.ts
@@ -0,0 +1,104 @@
+
+import { expect as expectCDK, haveResource, countResources, countResourcesLike } from '@aws-cdk/assert';
+import * as cdk from '@aws-cdk/core';
+import { EcsBfStack } from '../lib/ecs-bf-stack';
+import { EcsBaseStack } from '../lib/ecs-base-stack';
+import { RasaBot } from '../types';
+import { DefaultRepositories } from '../envs/environment';
+
+const envName = 'test';
+const subDomain = 'test';
+const domain = 'test.test';
+const region = 'test';
+const account = '0123456789'
+
+const defaultRepositories: DefaultRepositories = {
+    actionsRepository: 'test',
+    botfrontRepository: 'test',
+    rasaBotRepository: 'test',
+};
+let ecrRepos: RasaBot[] = [{rasaPort: 1, actionsPort: 2, projectId: 'veryrealid', customerName: 'veryrealcustomer'}];
+
+
+test('Create botfront-stack with one bot', () => {
+  const app = new cdk.App();
+  // WHEN
+  const basestack = new EcsBaseStack(app, 'MyBaseStack', {
+    envName,
+    subDomain,
+    ecrRepos,
+    domain,
+    env: {
+      region,
+      account
+    },
+    defaultRepositories
+  });
+  const teststack = new EcsBfStack(app, 'MyTestStack', {
+    envName,
+    domain,
+    env: {
+      region,
+      account
+    },
+    baseCertificate: basestack.baseCertificate,
+    baseCluster: basestack.baseCluster,
+    baseLoadbalancer: basestack.baseLoadBalancer,
+    baseVpc: basestack.baseVpc,
+    mongoSecret: basestack.mongoSecret
+  });
+  // THEN
+  expectCDK(teststack).to(countResources('AWS::ECS::TaskDefinition', 1)
+  .and(countResources('AWS::IAM::Role', 2))
+  .and(countResources('AWS::IAM::Policy', 1))
+  .and(countResources('AWS::ECS::Service', 1))
+  .and(countResources('AWS::ServiceDiscovery::Service', 1))
+  .and(countResources('AWS::EC2::SecurityGroup', 1))
+  .and(countResources('AWS::ElasticLoadBalancingV2::Listener', 1))
+  .and(countResources('AWS::ElasticLoadBalancingV2::TargetGroup', 1))
+  );
+});
+
+test('Create botfront-stack with two bots', () => {
+    const app = new cdk.App();
+    ecrRepos = [
+      {rasaPort: 1, actionsPort: 2, projectId: 'veryrealid', customerName: 'veryrealcustomer'},
+      {rasaPort: 3, actionsPort: 4, projectId: 'veryrealid2', customerName: 'veryrealcustomer2'}
+    ];
+
+    // WHEN
+    const basestack = new EcsBaseStack(app, 'MyBaseStack', {
+      envName,
+      subDomain,
+      ecrRepos,
+      domain,
+      env: {
+        region,
+        account
+      },
+      defaultRepositories
+    });
+    const teststack = new EcsBfStack(app, 'MyTestStack', {
+      envName,
+      domain,
+      env: {
+        region,
+        account
+      },
+      baseCertificate: basestack.baseCertificate,
+      baseCluster: basestack.baseCluster,
+      baseLoadbalancer: basestack.baseLoadBalancer,
+      baseVpc: basestack.baseVpc,
+      mongoSecret: basestack.mongoSecret
+    });
+    // THEN
+    expectCDK(teststack).to(countResources('AWS::ECS::TaskDefinition', 1)
+    .and(countResources('AWS::IAM::Role', 2))
+    .and(countResources('AWS::IAM::Policy', 1))
+    .and(countResources('AWS::ECS::Service', 1))
+    .and(countResources('AWS::ServiceDiscovery::Service', 1))
+    .and(countResources('AWS::EC2::SecurityGroup', 1))
+    .and(countResources('AWS::ElasticLoadBalancingV2::Listener', 1))
+    .and(countResources('AWS::ElasticLoadBalancingV2::TargetGroup', 1))
+    );
+});
