4ee677e1f8b93809d21e0a26c2b7321143ae9810
Tuomas Paulin <tuomas.paulin@gofore.com>
2021-12-03 15:49:49 +0200

    Removed hytebot-demo

--- infra/aws/bin/aws.ts
+++ infra/aws/bin/aws.ts
@@ -71,21 +71,6 @@ const demoRasaBots: RasaBot[] = [
     actionsPort: 5055,
     projectId: 'hH4Z8S7GXiHsp3PTP',
     customerName: 'demo-1'
-  }, 
-  {
-    rasaPort: 5005,
-    actionsPort: 5056,
-    projectId: 'h5W28PhKxRYYFkh2N',
-    customerName: 'hytebot-demo',
-    additionalConfig: {
-      intents: {
-        onerva: '/aloita{"oma_organisaatio": "onerva"}',
-        vamos: '/aloita{"oma_organisaatio": "vamos"}',
-        helsinkimissio: '/aloita{"oma_organisaatio": "helsinki missio"}',
-        poikienpuhelin: '/aloita{"oma_organisaatio": "poikien puhelin"}',
-        asemanlapset: '/aloita{"oma_organisaatio": "aseman lapset"}'      
-      }
-    }
   },
   {
     rasaPort: 5007,
