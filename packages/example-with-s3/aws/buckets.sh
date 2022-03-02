21c1b2a7e45c27b0e1667beddeb51d7ecad4a411
Tuomas Paulin <tuomas.paulin@gofore.com>
2022-03-02 10:03:28 +0200

    change localstack to moto

--- packages/example-with-s3/aws/buckets.sh
+++ packages/example-with-s3/aws/buckets.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Init localstack s3"
 awslocal s3 mb s3://testbucket
-awslocal s3 cp testfile s3://testbucket/languagemodels/testfile
+awslocal s3 cp /docker-entrypoint-initaws.d/testfile.txt s3://testbucket/languagemodels/testfile.txt
